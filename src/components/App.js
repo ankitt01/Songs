c2c18d47e254c95af537c0336b76cccd82f91d58
ankitt01 <ankitrawat0102@gmail.com>
2021-06-10 19:01:05 +0530

    reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {selecSong} from '../actions';
 
 function App() {
   return (
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+//Reducers 1) Song list reducer and 2) Selected song reducer
+
+const songsReducer = () => {
+    return [
+        { title: 'Numb', duration: '4:05' },
+        { title: 'In the end', duration: '3:45' },
+        { title: 'Castle of glass', duration: '1:45' },
+        { title: 'Breaking the habbit', duration: '2:45' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = NULL, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
